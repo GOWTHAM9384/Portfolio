21ad98dd76c8c7441217053dbca734ff4e2bf329
GOWTHAM9384 <gowthamaravinth00@gmail.com>
2026-02-13 13:30:53 +0530

    backend

--- server/src/server.js
+++ server/src/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
